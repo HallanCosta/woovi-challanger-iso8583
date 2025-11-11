1b45931056e6a07308093867c2bb7e9eb78a38b6
HallanCosta <hallan.costa1@hotmail.com>
2025-11-11 18:43:43 -0300

    feat: configure pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,24 +1,34 @@
 module.exports = {
   apps: [
     {
-      name: "iso8583-web",
-      cwd: "./",
+      name: "iso8583-server",
+      cwd: "./server",
       script: "pnpm",
-      args: "preview --host 0.0.0.0 --port 4174 --strictPort",
-      env: { NODE_ENV: "production" },
+      args: "dev",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      },
       autorestart: true,
       watch: false,
       max_memory_restart: "500M",
     },
     {
-      name: "iso8583-server",
-      cwd: "./",
+      name: "iso8583-web",
+      cwd: "./web",
       script: "pnpm",
       args: "dev",
-      env: { NODE_ENV: "production" },
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      },
       autorestart: true,
       watch: false,
       max_memory_restart: "500M",
     },
   ],
-}
+};
