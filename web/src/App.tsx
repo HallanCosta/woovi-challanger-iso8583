ae6e75ab7ddef7cf60a1164c6e2c1d60302c5473
HallanCosta <hallan.costa1@hotmail.com>
2025-11-10 23:43:19 -0300

    feat: add react-slot

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
